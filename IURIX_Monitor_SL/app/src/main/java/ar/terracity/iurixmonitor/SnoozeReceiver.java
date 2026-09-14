9dd94afb6ba1041d4e2d93b4020fb09e35f50bc9
YungoR <96013564+YungoR@users.noreply.github.com>
2026-09-14 12:13:29 -0300

    Add 30 minute snooze receiver

---
+++ IURIX_Monitor_SL/app/src/main/java/ar/terracity/iurixmonitor/SnoozeReceiver.java
@@ -0,0 +1,12 @@
+package ar.terracity.iurixmonitor;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SnoozeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        AlarmScheduler.scheduleSnooze(context, 30);
+    }
+}
